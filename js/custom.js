e2e55d3c5a2bbeba4a8f2dd39f31872648fddfa3
Nicolas Haas <71752432+NicHa1986@users.noreply.github.com>
2023-05-04 19:34:12 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -319,3 +319,12 @@ $(function () {
 
 
 });
+
+const readMoreBtn = document.querySelector('.read-more-button');
+const moreText = document.querySelector('.read-more');
+
+readMoreBtn.addEventListener('click',(e) => {
+  moreText.classList.toggle('read-more');
+  
+  // Consider changing the button text to collapse or removing it altogether perhaps?
+})
