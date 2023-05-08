aaca2a619433c0edbd60504297ac0a47122c214c
Nicolas Haas <71752432+NicHa1986@users.noreply.github.com>
2023-05-08 09:48:05 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const readMoreBtn = document.querySelector('.read-more-button');
 const moreText = document.querySelector('.read-more');
 
-readMoreBtn.addEventListener('click',function() {
+readMoreBtn.addEventListener('click',function(e) {
   moreText.classList.toggle('read-more');
   
   // Consider changing the button text to collapse or removing it altogether perhaps?
-})
+});
